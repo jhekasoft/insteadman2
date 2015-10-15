f82bb42b7d30fb46e4fa85db77da22d02e19b550
jhekasoft <jheka@mail.ru>
2015-10-16 01:49:47 +0300

    Interpreter finder

--- src/lib/interpreter_finder.js
+++ src/lib/interpreter_finder.js
@@ -1,18 +1,60 @@
 "use strict";
 
+var fs = require('fs');
+var childProcess = require('child_process');
+
 class InsteadInterpreterFinder {
 
     constructor() {
         this.downloadLink = 'http://instead.syscall.ru/ru/download/';
+        this.exactFilePaths = [];
+    }
+
+    getDownloadLink() {
+        return this.downloadLink;
+    }
+
+    findInterpreter() {
+        var foundPath = false;
+        var here = this;
+        this.exactFilePaths.forEach(function(path) {
+            if (here.checkInterpreterPath(path)) {
+                foundPath = path;
+            }
+        });
+
+        return foundPath;
+    }
+
+    checkInterpreterPath(path) {
+        try {
+            return fs.statSync(path).isFile();
+        } catch (err) {
+            return false;
+        }
+    }
+
+    checkInterpreter(interpreterCommand) {
+        childProcess.exec(interpreterCommand + " -version", function(error, stdout, stderr) {
+            console.log([error, stdout, stderr]);
+        });
+    }
+}
+
+class InsteadInterpreterFinderMac extends InsteadInterpreterFinder {
+    constructor() {
+        super();
+        this.exactFilePaths = [
+            '/Applications/Instead.app/Contents/MacOS/sdl-instead'
+        ];
     }
 
 }
 
 class InsteadInterpreterFinderFreeUnix extends InsteadInterpreterFinder {
 
-    findInterpreter() {
-        return 'Unix';
-    }
+
 }
 
+module.exports.InsteadInterpreterFinderMac = InsteadInterpreterFinderMac;
 module.exports.InsteadInterpreterFinderFreeUnix = InsteadInterpreterFinderFreeUnix;
